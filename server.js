398fcfa2d62981ce010ac165d87e1b27a163539b
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-11-23 15:11:18 -0500

    completed part one of the lab

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var http = require("http");
 var toDo = require("./to-do.js");
 
-console.log(toDo);
-
 http.createServer(onRequest).listen(3000);
 
 function onRequest(request, response) {
     response.writeHead(200, {"content-type": "text/plain"});
-    response.write("displaying");
+    var x = Math.floor(Math.random() * toDo.items.tasks.length);
+    response.write(toDo.items.tasks[x]);
     response.end();
 }
